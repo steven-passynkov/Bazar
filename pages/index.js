02a159284f3c6fcbab4c887b1b30010e7dfcd21b
Steven Passynkov <passynkovsteven@gmail.com>
2021-03-22 20:38:29 -0400

    carousel

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Nav_bar from "../components/nav-bar.js";
+import Carousel_PayedPost from "../components/carousel.js"
 
 export default function Home() {
   return(
-    <Nav_bar />
+    <div>
+    <Nav_bar/>
+    <Carousel_PayedPost/>
+    </div>
   )
 }
