88517600c39c7e8a9719861e0130629f35c8eb07
Steven Passynkov <passynkovsteven@gmail.com>
2021-04-15 20:56:09 -0400

    added css to nav-bar

--- pages/home.js
+++ pages/home.js
@@ -3,37 +3,50 @@ import Carousel_PayedPost from "../components/carousel-paidAds";
 import Carousel_Items from "../carousel-items";
 import Footer from "../components/footer";
 import Card from "react-bootstrap/Card";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import Home_css from "../styles/Home.module.css";
 
 export default function Home_Page() {
   return (
     <div>
       <Nav_bar />
-      <Card className="text-center" style={{margin:'4rem'}}>
-        <Card.Header style={{color:'#00008b'}}>
-          <strong>Trending</strong>
-        </Card.Header>
-        <Card.Body>
-          <Carousel_PayedPost
-            numberItemsDesktop={3}
-            numberItemsTable={2}
-            numberItemsMobile={1}
-            items={Carousel_Items.trending_items}
-          />
-        </Card.Body>
-      </Card>
-      <Card className="text-center" style={{margin:'4rem'}}>
-        <Card.Header style={{color:'#00008b'}}>
-          <strong>Auto</strong>
-        </Card.Header>
-        <Card.Body>
-          <Carousel_PayedPost
-            numberItemsDesktop={4}
-            numberItemsTable={2}
-            numberItemsMobile={1}
-            items={Carousel_Items.auto_items}
-          />
-        </Card.Body>
-      </Card>
+      <div className={Home_css.box}>
+<div style={{margin:"3rem"}}>
+        <Jumbotron>
+          <h1>Sell Now!</h1>
+          <p>
+            <Button variant="primary">Sell</Button>
+          </p>
+        </Jumbotron>
+</div>
+        <Card className="text-center" style={{ margin: "4rem" }}>
+          <Card.Header style={{ color: "#00008b" }}>
+            <strong>Trending</strong>
+          </Card.Header>
+          <Card.Body>
+            <Carousel_PayedPost
+              numberItemsDesktop={3}
+              numberItemsTable={2}
+              numberItemsMobile={1}
+              items={Carousel_Items.trending_items}
+            />
+          </Card.Body>
+        </Card>
+        <Card className="text-center" style={{ margin: "4rem" }}>
+          <Card.Header style={{ color: "#00008b" }}>
+            <strong>Auto</strong>
+          </Card.Header>
+          <Card.Body>
+            <Carousel_PayedPost
+              numberItemsDesktop={4}
+              numberItemsTable={2}
+              numberItemsMobile={1}
+              items={Carousel_Items.auto_items}
+            />
+          </Card.Body>
+        </Card>
+      </div>
       <Footer />
     </div>
   );
