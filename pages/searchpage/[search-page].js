aee46cad9a9b983d04908601d635d037eb72a085
Steven Passynkov <passynkovsteven@gmail.com>
2021-04-27 16:41:58 -0400

    a css bug

--- pages/searchpage/[search-page].js
+++ pages/searchpage/[search-page].js
@@ -3,20 +3,35 @@ import Footer from "../../components/footer";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import Card from "react-bootstrap/Card";
 import Search_results from "../../search-results.json";
+import Search_results_css from "../../styles/Search_results.module.css";
+import { useRouter } from "next/router";
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
 
 export default function search_page() {
+  const router = useRouter();
+  const { search } = router.query;
+
   return (
     <div>
+      <div>
       <Nav_bar />
       <Breadcrumb>
         <Breadcrumb.Item href="../../">Home</Breadcrumb.Item>
-        <Breadcrumb.Item href="#">random</Breadcrumb.Item>
-        <Breadcrumb.Item href="#">random</Breadcrumb.Item>
-        <Breadcrumb.Item active>random</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">Catagory: {}</Breadcrumb.Item>
+        <Breadcrumb.Item href="#">Region: {}</Breadcrumb.Item>
+        <Breadcrumb.Item active>{search}</Breadcrumb.Item>
       </Breadcrumb>
+      </div>
+      <Container fluid>
+        <Col>
       {Search_results.map((el) => (
-          <div>
-          <Card variant="light" key={el.id}>
+        <div>
+          <Card
+            className={Search_results_css.results}
+            variant="light"
+            key={el.id}
+          >
             <Card.Img variant="top" src="" />
             <Card.Body>
               <Card.Title>
@@ -25,8 +40,15 @@ export default function search_page() {
               <Card.Text>Text</Card.Text>
             </Card.Body>
           </Card>
-          </div>
-        ))}
+        </div>
+      ))}
+      </Col>
+      <div className={Search_results_css.fliter}>
+        <Card>
+          <Card.Title>Fliter:</Card.Title>
+        </Card>
+      </div>
+      </Container>
     </div>
   );
 }
