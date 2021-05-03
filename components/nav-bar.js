d9b9fdc6ec7fb195ad3b4f0e62c9edfc0a7563e2
Steven Passynkov <passynkovsteven@gmail.com>
2021-05-03 17:03:48 -0400

    made changes to the nav bar

--- components/nav-bar.js
+++ components/nav-bar.js
@@ -7,11 +7,12 @@ import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import FormControl from "react-bootstrap/FormControl";
 import Card from "react-bootstrap/Card";
+import InputGroup from "react-bootstrap/InputGroup";
 import { useRef } from "react";
 import { useRouter } from "next/router";
 
 export default function Nav_bar() {
-  const titleRef = useRef(0)
+  const titleRef = useRef(0);
   const router = useRouter();
 
   const handleSubmit = (event) => {
@@ -23,37 +24,50 @@ export default function Nav_bar() {
   return (
     <div>
       <Card style={{ background: "#00008b" }} />
+      <div>
       <Navbar expand="lg" className="justify-content-between">
         <Nav className="mr-auto">
           <Navbar.Brand href="/.." variant="light">
             Bozar
           </Navbar.Brand>
+          <div>
+            <InputGroup className="searchbar">
+              <FormControl
+                placeholder="What do you want"
+                aria-label="What do you want"
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <Button variant="light" className="searchb">
+                  Search
+                </Button>
+              </InputGroup.Append>
 
-          <Form inline className="mr-sm-2" onSubmit={handleSubmit}>
-            <FormControl
-              htmlSize={100}
-              type="text"
-              placeholder="What are you looking for?"
-              className="mr-sm-2"
-              onChange={(event) => titleRef.current = event.target.value}
-            />
-            <Button variant="light" type="submit" className="searchb">
-              Search
-            </Button>
-          </Form>
+              <NavDropdown
+                title="Categories"
+                id="basic-nav-dropdown"
+                className="dropdown"
+              >
+                <NavDropdown.Item href="#action/3.1">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+              </NavDropdown>
 
-          <NavDropdown title="Categories" id="basic-nav-dropdown" className="dropdown">
-            <NavDropdown.Item href="#action/3.1">Something</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Something</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          </NavDropdown>
+              <Button variant="light" className="sellbtn">
+                Sell
+                <img src="" />
+              </Button>
+            </InputGroup>
+          </div>
         </Nav>
-
-        <Button variant="light" className="sellbtn">
-          Sell
-          <img src="" />
-        </Button>
       </Navbar>
+      </div>
     </div>
   );
 }
