85c144a4c1d2bae67fce0bb046122db2ef7ce806
Steven Passynkov <passynkovsteven@gmail.com>
2021-06-04 13:00:37 -0400

    fixed bug

--- pages/login.js
+++
@@ -1,9 +0,0 @@
-import { Auth0Provider } from "@auth0/auth0-react";
-
-const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
-};
-
-export default LoginButton;
