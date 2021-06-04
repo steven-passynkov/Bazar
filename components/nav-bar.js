b9819ee07801d1c4bbd70bf7cc0a33c8579d5302
Steven Passynkov <passynkovsteven@gmail.com>
2021-06-04 12:44:15 -0400

    added login and logout

--- components/nav-bar.js
+++ components/nav-bar.js
@@ -5,13 +5,16 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import FormControl from "react-bootstrap/FormControl";
 import Card from "react-bootstrap/Card";
 import InputGroup from "react-bootstrap/InputGroup";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useMediaQuery } from "react-responsive";
+import { useUser } from "@auth0/nextjs-auth0";
 
 export default function Nav_bar() {
   const titleRef = useRef(0);
   const router = useRouter();
+  const { user, error, isLoading } = useUser();
+  const [log, setLog] = useState(true);
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -36,106 +39,223 @@ export default function Nav_bar() {
     return isNotMobile ? children : null;
   };
 
-  return (
-    <div>
-      <Desktop>
-        <Card style={{ background: "#00008b" }} />
-        <div>
-          <Navbar expand="lg" className="justify-content-between">
-            <Nav className="mr-auto">
-              <Navbar.Brand href="/.." variant="light">
-                Bozar
-              </Navbar.Brand>
-              <div>
-                <InputGroup className="searchbar">
-                  <FormControl
-                    placeholder="What do you want"
-                    aria-label="What do you want"
-                    aria-describedby="basic-addon2"
-                    onSubmit={handleSubmit}
-                  />
-                  <InputGroup.Append>
-                    <Button variant="light" className="searchb" onClick={handleSubmit}>
-                      Search
+  if (user) {
+    return (
+      <div>
+        <Desktop>
+          <Card style={{ background: "#00008b" }} />
+          <div>
+            <Navbar expand="lg" className="justify-content-between">
+              <Nav className="mr-auto">
+                <Navbar.Brand href="/.." variant="light">
+                  Bozar
+                </Navbar.Brand>
+                <div>
+                  <InputGroup className="searchbar">
+                    <FormControl
+                      placeholder="What do you want"
+                      aria-label="What do you want"
+                      aria-describedby="basic-addon2"
+                      onSubmit={handleSubmit}
+                    />
+                    <InputGroup.Append>
+                      <Button
+                        variant="light"
+                        className="searchb"
+                        onClick={handleSubmit}
+                      >
+                        Search
+                      </Button>
+                    </InputGroup.Append>
+                    <NavDropdown
+                      title="Categories"
+                      id="basic-nav-dropdown"
+                      className="dropdown"
+                    >
+                      <NavDropdown.Item href="#action/3.1">
+                        Something
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">
+                        Something
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">
+                        Something
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                    <Button
+                      variant="light"
+                      className="sellbtn"
+                      href={"../post-ad/new-ad"}
+                    >
+                      Sell
+                      <img src="" />
                     </Button>
-                  </InputGroup.Append>
-
-                  <NavDropdown
-                    title="Categories"
-                    id="basic-nav-dropdown"
-                    className="dropdown"
-                  >
-                    <NavDropdown.Item href="#action/3.1">
-                      Something
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                      Something
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">
-                      Something
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                  <Button variant="light" className="sellbtn" href={"../post-ad/new-ad"}>
+                    <Button href="api/auth/logout">Logout</Button>
+                  </InputGroup>
+                </div>
+              </Nav>
+            </Navbar>
+          </div>
+        </Desktop>
+        <Mobile>
+          <Card style={{ background: "#00008b" }} />
+          <div>
+            <Navbar expand="lg" className="justify-content-between">
+              <Nav className="mr-auto">
+                <Navbar.Brand href="/.." variant="light">
+                  Bozar
+                </Navbar.Brand>
+                <div>
+                  <Button variant="light" className="sellbtn">
                     Sell
                     <img src="" />
                   </Button>
-                </InputGroup>
-              </div>
-            </Nav>
-          </Navbar>
-        </div>
-      </Desktop>
-      <Mobile>
-        <Card style={{ background: "#00008b" }} />
-        <div>
-          <Navbar expand="lg" className="justify-content-between">
-            <Nav className="mr-auto">
-              <Navbar.Brand href="/.." variant="light">
-                Bozar
-              </Navbar.Brand>
-              <div>
-                <Button variant="light" className="sellbtn">
-                  Sell
-                  <img src="" />
-                </Button>
-              </div>
-              <div>
-                <InputGroup className="searchbar">
-                  <FormControl
-                    placeholder="What do you want"
-                    aria-label="What do you want"
-                    aria-describedby="basic-addon2"
-                  />
-                  <InputGroup.Append>
-                    <Button variant="light" className="searchb">
-                      Search
-                    </Button>
-                  </InputGroup.Append>
+                  <Button href="api/auth/logout">Logout</Button>
+                </div>
+                <div>
+                  <InputGroup className="searchbar">
+                    <FormControl
+                      placeholder="What do you want"
+                      aria-label="What do you want"
+                      aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Append>
+                      <Button variant="light" className="searchb">
+                        Search
+                      </Button>
+                    </InputGroup.Append>
 
-                  <NavDropdown
-                    title="Categories"
-                    id="basic-nav-dropdown"
-                    className="dropdown"
-                  >
-                    <NavDropdown.Item href="#action/3.1">
-                      Something
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                      Something
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">
-                      Something
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                </InputGroup>
-                <Navbar href="api/auth/login" variant="light">
-                Bozar
-              </Navbar>
-              </div>
-            </Nav>
-          </Navbar>
-        </div>
-      </Mobile>
-    </div>
-  );
+                    <NavDropdown
+                      title="Categories"
+                      id="basic-nav-dropdown"
+                      className="dropdown"
+                    >
+                      <NavDropdown.Item href="#action/3.1">
+                        Something
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">
+                        Something
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">
+                        Something
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                  </InputGroup>
+                </div>
+              </Nav>
+            </Navbar>
+          </div>
+        </Mobile>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Desktop>
+          <Card style={{ background: "#00008b" }} />
+          <div>
+            <Navbar expand="lg" className="justify-content-between">
+              <Nav className="mr-auto">
+                <Navbar.Brand href="/.." variant="light">
+                  Bozar
+                </Navbar.Brand>
+                <div>
+                  <InputGroup className="searchbar">
+                    <FormControl
+                      placeholder="What do you want"
+                      aria-label="What do you want"
+                      aria-describedby="basic-addon2"
+                      onSubmit={handleSubmit}
+                    />
+                    <InputGroup.Append>
+                      <Button
+                        variant="light"
+                        className="searchb"
+                        onClick={handleSubmit}
+                      >
+                        Search
+                      </Button>
+                    </InputGroup.Append>
+                    <NavDropdown
+                      title="Categories"
+                      id="basic-nav-dropdown"
+                      className="dropdown"
+                    >
+                      <NavDropdown.Item href="#action/3.1">
+                        Something
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">
+                        Something
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">
+                        Something
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                    <Button
+                      variant="light"
+                      className="sellbtn"
+                      href={"../post-ad/new-ad"}
+                    >
+                      Sell
+                      <img src="" />
+                    </Button>
+                    <Button href="api/auth/login">Login</Button>
+                  </InputGroup>
+                </div>
+              </Nav>
+            </Navbar>
+          </div>
+        </Desktop>
+        <Mobile>
+          <Card style={{ background: "#00008b" }} />
+          <div>
+            <Navbar expand="lg" className="justify-content-between">
+              <Nav className="mr-auto">
+                <Navbar.Brand href="/.." variant="light">
+                  Bozar
+                </Navbar.Brand>
+                <div>
+                  <Button variant="light" className="sellbtn">
+                    Sell
+                    <img src="" />
+                  </Button>
+                  <Button href="api/auth/login">Login</Button>
+                </div>
+                <div>
+                  <InputGroup className="searchbar">
+                    <FormControl
+                      placeholder="What do you want"
+                      aria-label="What do you want"
+                      aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Append>
+                      <Button variant="light" className="searchb">
+                        Search
+                      </Button>
+                    </InputGroup.Append>
+
+                    <NavDropdown
+                      title="Categories"
+                      id="basic-nav-dropdown"
+                      className="dropdown"
+                    >
+                      <NavDropdown.Item href="#action/3.1">
+                        Something
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.2">
+                        Something
+                      </NavDropdown.Item>
+                      <NavDropdown.Item href="#action/3.3">
+                        Something
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                  </InputGroup>
+                </div>
+              </Nav>
+            </Navbar>
+          </div>
+        </Mobile>
+      </div>
+    );
+  }
 }
