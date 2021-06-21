be4e12445e90208491698a98208dd02c04d3bb57
Steven Passynkov <passynkovsteven@gmail.com>
2021-06-21 07:17:19 -0400

    added users data and it works just need to optimise

---
+++ pages/api/token.js
@@ -0,0 +1,42 @@
+var axios = require("axios").default;
+//const redis = require("redis").default;
+
+//const redisClient = redis.createClient(6379)
+
+export default async (req, res) => {
+  var options = {
+    method: "POST",
+    url: "https://dev-y75e739g.us.auth0.com/oauth/token",
+    headers: { 'content-type': 'application/json' },
+    data: {
+      client_id: "k5pSurhO0St2eOQbr4gu7zsDhL2JLruX",
+      client_secret:
+        "uc5rjYTZraWrxxvWkH_lPDlvLaRflxxNVB8BKdRRR0rlEkz6r8uQAv3xIsERe99m",
+      audience: "https://dev-y75e739g.us.auth0.com/api/v2/",
+      grant_type: "client_credentials",
+    },
+  };
+  try {
+    const data = await axios.request(options);
+    console.log(data.data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).end(e.message);
+  }
+
+ /* app.get("/api/token", (req, res) => {
+      
+    try {
+        redisClient.get(token, (err, data) => {
+          if (err) {
+            console.error(err);
+            throw err;
+          }
+          if (data) {
+            console.log("User successfully retrieved from Redis");
+    
+            res.status(200).send(JSON.parse(data));      
+  } catch (e) {
+    console.error(e);
+    res.status(500).send({ error: e.message });*/
+};
