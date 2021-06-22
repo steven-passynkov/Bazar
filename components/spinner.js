7dbd4d8b0ab71aeceb69310744407203cbfcece7
Steven Passynkov <passynkovsteven@gmail.com>
2021-06-22 19:50:22 -0400

    added new spinner

--- components/spinner.js
+++ components/spinner.js
@@ -2,8 +2,6 @@ export default function () {
   return (
     <div className="stage">
       <div className="dot" />
-      <div className="ping" />
-      <div className="logo" />
     </div>
   );
 }
