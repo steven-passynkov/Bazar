e0ef925329b4c4143e9a969240e2691a71c49e5b
Steven Passynkov <passynkovsteven@gmail.com>
2021-09-15 15:38:50 -0400

    fixed bug

--- pages/signin.js
+++ pages/signin.js
@@ -1,8 +1,17 @@
 import Card from "react-bootstrap/Card"
+import { useState, useEffect } from "react";
 
-export default function login({ user }) {
+export default function login({  }) {
+  const user = ({session, supabase}) => {
+    const [loggedIn, setLoggedIn] = useState(false);
+  
+    useEffect(() => {
+      setLoggedIn(!!session);
+    }, [session]);
+  };
+  console.log(user)
   return (
-    <div classname="backround">
+    <div className="backround">
       <div> 
       <Card></Card>
       </div>
