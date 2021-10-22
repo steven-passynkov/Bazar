39f89bf6536e0235192a0dca6dc51507efc3f7c9
Steven Passynkov <passynkovsteven@gmail.com>
2021-10-22 19:03:26 -0400

    deleted auth0

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    env: {
-      AUTH0_CLIENT_ID:'Eo8NoQilc5f1NIx8uHXJ4v50ltWC2zSj',
-      AUTH0_CLIENT_SECRET:'vPxVxB5SpvQ_lyoJVigTuqYl97SX-D2ypLWZ_mXDUMc9juCsV_Re3M0Aok1FIWIp'
-    },
-    images: {
-      domains: ['s.gravatar.com', 'lh3.googleusercontent.com'],
-    },
-  }
